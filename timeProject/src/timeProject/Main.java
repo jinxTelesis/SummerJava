8e30fe293f826b6a35d544c1748869b6c029ef3d
jinxtelesis <dremon7@hotmail.com>
2018-05-25 00:06:32 -0400

    update on constructors

--- timeProject/src/timeProject/Main.java
+++ timeProject/src/timeProject/Main.java
@@ -3,7 +3,7 @@ package timeProject;
 public class Main {
 
 	public static void main(String[] args) {
-		SecondClass SecObject = new SecondClass();
+		tuna SecObject = new tuna();
 		System.out.println(SecObject.toMilitary());
 		
 		SecObject.setTime(10, 50, 10);
