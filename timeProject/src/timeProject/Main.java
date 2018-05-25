6f3aeeec9df87b7315aa58a755c635e2e20838dc
jinxtelesis <dremon7@hotmail.com>
2018-05-25 16:18:56 -0400

    First java GUI

--- timeProject/src/timeProject/Main.java
+++ timeProject/src/timeProject/Main.java
@@ -1,14 +1,21 @@
 package timeProject;
 import java.util.EnumSet;
+import javax.swing.JOptionPane;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		tuna tunaObject = new tuna();
-		potpie potObject = new potpie();
 		
-		tunaObject.eat();
-		potObject.eat();
+		String fn = JOptionPane.showInputDialog("Enter first number");
+		String sn = JOptionPane.showInputDialog("Enter second number");
+		
+		int num1 = Integer.parseInt(fn);
+		int num2 = Integer.parseInt(sn);
+		int sum = num1 + num2;
+		
+		JOptionPane.showMessageDialog(null, "The answer is " + sum,
+				"Title", JOptionPane.PLAIN_MESSAGE);
 	}
 	
 }
--- timeProject/src/timeProject/tuna.java
+++ timeProject/src/timeProject/tuna.java
@@ -2,6 +2,9 @@ package timeProject;
 
 public class tuna extends Food {
 	
-	
+	public void eat()
+	{
+		System.out.println("I am the wall of methods");
+	}
 	
 }
