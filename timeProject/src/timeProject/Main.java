f0750f340b0c8d0c0e0edc23622414710b65dafa
jinxtelesis <dremon7@hotmail.com>
2018-05-24 22:21:49 -0400

    initial time commit

---
+++ timeProject/src/timeProject/Main.java
@@ -0,0 +1,13 @@
+package timeProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SecondClass SecObject = new SecondClass();
+		System.out.println(SecObject.toMilitary());
+		
+		
+
+	}
+
+}
