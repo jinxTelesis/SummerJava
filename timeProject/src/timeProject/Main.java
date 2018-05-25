8ae0346a3e33c340ad4c19c3136330295009b214
jinxtelesis <dremon7@hotmail.com>
2018-05-25 00:30:28 -0400

    this as a default to toString()

--- timeProject/src/timeProject/Main.java
+++ timeProject/src/timeProject/Main.java
@@ -3,10 +3,7 @@ package timeProject;
 public class Main {
 
 	public static void main(String[] args) {
-		tuna SecObject = new tuna();
-		System.out.println(SecObject.toMilitary());
-		
-		SecObject.setTime(10, 50, 10);
+		potpie potObject = new potpie(4,5,6);
 		
 		
 		
---
+++ timeProject/src/timeProject/potpie.java
@@ -0,0 +1,22 @@
+package timeProject;
+
+public class potpie {
+	private int month;
+	private int day;
+	private int year;
+	
+	public potpie(int m, int d, int y) {
+		month = m;
+		day = d;
+		this.year = y;
+		
+		System.out.printf("The contstructor for this is %s\n", this);
+	}
+	
+	public String toString()
+	{
+		return String.format("%d/%d/%d", month, year, day);
+	}
+	
+	
+}
