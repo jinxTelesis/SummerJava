7868d49599966f3b2b798a3346f39f0143a0f645
jinxtelesis <dremon7@hotmail.com>
2018-05-26 11:03:21 -0400

    edit to gui

--- timeProject/src/timeProject/Animal.java
+++
@@ -1,9 +0,0 @@
-package timeProject;
-
-public class Animal {
-	
-	public void noise() {
-		System.out.println("Animals dont make noise");
-	}
-
-}
--- timeProject/src/timeProject/Fish.java
+++
@@ -1,7 +0,0 @@
-package timeProject;
-
-public class Fish extends Animal {
-	public void noise() {
-		System.out.println("glurp slurp");
-	}
-}
