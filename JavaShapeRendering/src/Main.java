6bdbb54d84d3477e4480484dfe76f628a831bf4d
jinxtelesis <dremon7@hotmail.com>
2018-05-28 17:45:46 -0400

    doodle

--- JavaShapeRendering/src/Main.java
+++ JavaShapeRendering/src/Main.java
@@ -1,8 +1,20 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JFrame f = new JFrame("Title");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ShapeGen R = new ShapeGen();
+		R.setBackground(Color.WHITE);
+		f.add(R);
+		f.setSize(500,270);
+		f.setVisible(true);
+		
 
 	}
 
