11f9446d5555cf7d98658c24293cd3703896e4a1
jinxtelesis <dremon7@hotmail.com>
2018-05-28 14:17:54 -0400

    update

--- MouseEvents/src/Gui.java
+++ MouseEvents/src/Gui.java
@@ -4,4 +4,21 @@ import javax.swing.*;
 
 public class Gui extends JFrame {
 	private JPanel mousepanel;
-}
+	private JLabel statusbar;
+	
+	public Gui() {
+		super("title");
+		
+		mousepanel = new JPanel();
+		mousepanel.setBackground(Color.WHITE);
+		add(mousepanel, BorderLayout.CENTER);
+		
+		statusbar = new JLabel("default");
+		add(statusbar, BorderLayout.SOUTH);
+		
+		Handlerclass handler = new Handlerclass();
+		mousepanel.addMouseListener(handler);
+		mousepanel.addMouseMotionListener(handler);
+	}
+	
+	
