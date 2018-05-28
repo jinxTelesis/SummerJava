86948190acd90c14513828b1a06ffc7290d2e738
jinxtelesis <dremon7@hotmail.com>
2018-05-28 17:11:13 -0400

    colorselection

---
+++ JavaColors/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Colors2 blah = new Colors2();
+		blah.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
