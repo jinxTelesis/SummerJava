721b4100236c031ebe80e6b7ad3348ce4cfffede
jinxtelesis <dremon7@hotmail.com>
2018-05-29 16:10:32 -0400

    Initial generic commit

---
+++ Generic/src/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Integer[] iray = {1, 2, 3, 4};
+		Character[] cray = {'b','d','r','r'};
+		
+		printMe(iray);
+		printMe(cray);
+
+	}
+	
+	public static void printMe(Integer[] i) {
+		for(Integer x : i)
+			System.out.printf("%s ", x);
+		System.out.println();
+	}
+	
+	public static void printMe(Character[] i) {
+		for(Character x : i)
+			System.out.printf("%s ", x);
+		System.out.println();
+	}
+	
+	// generic method
+	public static <T> void printMe(T[] x) {
+		for(T b : x)
+			System.out.printf("%s ", b);
+	}
+
+
+}
