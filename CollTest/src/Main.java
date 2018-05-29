79355fb82c935b6a4945292c747ac0492fe50b37
jinxtelesis <dremon7@hotmail.com>
2018-05-29 12:54:41 -0400

    update 2

--- CollTest/src/Main.java
+++ CollTest/src/Main.java
@@ -4,19 +4,23 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		int update = 10;
 		
-		String[] identifier = {"eggs", "lasers","hats", "pie"};
+		String[] things = {"eggs", "lasers", "hats", "rope"};
 		List<String> list1 = new ArrayList<String>();
 		
-		for(String x: identifier)
+		for(String x : things)
 			list1.add(x);
 		
 		String[] morethings = {"lasers", "hats"};
 		List<String> list2 = new ArrayList<String>();
 		
-		for(String y: morethings)
-			list1.add(y);
+		for (String y : morethings)
+			list2.add(y);
+		
+		for(int i = 0;i<list1.size();i++)
+		{
+			System.out.printf("%s ", list1.get(i));
+		}
 		
 
 	}
