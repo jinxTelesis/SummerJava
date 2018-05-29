23d1de710a0cb7c1a6b0b8414ab020a1b9f8feb3
jinxtelesis <dremon7@hotmail.com>
2018-05-29 13:59:08 -0400

    update to collections fill / copy

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -5,14 +5,39 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String[] crap = {"shoes", "apples", "shebait", "mooch", "look"};
-		List<String> ListL = Arrays.asList(crap);
-		
-		Collections.sort(ListL);
-		System.out.printf("%s\n", ListL);
-		
-		Collections.sort(ListL, Collections.reverseOrder());
-		System.out.printf("%s\n", ListL);
+		//create an array and convert to list
+		Character[] rayball = {'p', 'w', 'O'};
+		List<Character> ListL = Arrays.asList(rayball);
+		System.out.println("List is : ");
+		output(ListL); // fill in method later
 
+		//Reverse and print out the list
+		Collections.reverse(ListL);
+		System.out.println("After reverse : ");
+		output(ListL);
+		
+		//create new array and a new list
+		Character[] newRRRRRay = new Character[3];
+		List<Character> ListTwo = Arrays.asList(newRRRRRay);
+		
+		// copy contents of list into list copy
+		Collections.copy(ListTwo, ListL);
+		System.out.println("Copy of list : ");
+		output(ListTwo);
+		
+		// fill collection with stuff
+		Collections.fill(ListTwo,'A');
+		System.out.println("After filling the list : ");
+		output(ListTwo);
+	}
+	
+	// print method
+	private static void output(List<Character> ListL) {
+		
+		for(Character work: ListL)
+		{
+			System.out.printf("%s ", work);
+		}
+		System.out.println();
 	}
 }
