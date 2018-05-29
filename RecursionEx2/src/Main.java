08d9842b4f288a604b90d55b7eda62068beca530
jinxtelesis <dremon7@hotmail.com>
2018-05-29 16:33:23 -0400

    applet simple

--- RecursionEx2/src/Main.java
+++ RecursionEx2/src/Main.java
@@ -1,10 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class Main {
-
-	public static void main(String[] args) {
-		String ten = new String();
-		ten = "yo this is legit";
-
+public class Main extends JApplet{
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		g.drawString("wow this acutally worked?", 25, 25);
 	}
-
 }
