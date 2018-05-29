5d78a350e7d4cd75405edf89d251551639b3f7df
jinxtelesis <dremon7@hotmail.com>
2018-05-29 15:11:37 -0400

    initial que

--- StackEx/src/Main.java
+++ StackEx/src/Main.java
@@ -4,27 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Stack<String> stack = new Stack<String>();
-		stack.push("bottom");
-		printStack(stack);
-		stack.push("second");
-		printStack(stack);
-		stack.push("third");
-		printStack(stack);
+		PriorityQueue<String> vlad = new PriorityQueue<String>();
 		
-		stack.pop();
-		printStack(stack);
-		stack.pop();
-		printStack(stack);
-		stack.pop();
-		printStack(stack);
+		vlad.offer("first");
+		vlad.offer("second");
+		vlad.offer("third");
+		
+		System.out.printf("%s ", vlad);
+		System.out.println();
 	}
 	
-	private static void printStack(Stack<String> Strring) {
-		if (Strring.isEmpty())
-			System.out.println("Nothing in stack");
-		else 
-			System.out.printf("%s TOP\n" , Strring);
-	}
+	
 
 }
