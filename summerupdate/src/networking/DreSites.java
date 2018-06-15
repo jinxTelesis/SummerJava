db940a397dc007520e6eba51e0f4c441a9c2769b
jinxtelesis <dremon7@hotmail.com>
2018-06-15 16:02:08 -0400

    Hashmaps containing hashmaps

--- summerupdate/src/networking/DreSites.java
+++ summerupdate/src/networking/DreSites.java
@@ -1,15 +1,24 @@
 package networking;
 
+import java.applet.AppletContext;
+import java.awt.BorderLayout;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JApplet;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class DreSites extends JApplet {
 	
 	private HashMap<String, URL> websiteInfo;
+	private HashMap<String, URL> blahblah;
+	private HashMap<String, String> boop;
+	private HashMap<String, HashMap> moop;
+	
 	private ArrayList<String> titles;
 	private JList mainList;
 	
@@ -17,6 +26,28 @@ public class DreSites extends JApplet {
 	public void init() {
 		websiteInfo = new HashMap<String, URL>();
 		titles = new ArrayList<String>();
+		
+		grabHTMLInfo();
+		add(new JLabel("What website do you want to vist?"), BorderLayout.NORTH);
+		mainList = new JList(titles.toArray());
+		
+		mainList.addListSelectionListener(
+				// anoyomus inner class
+				new ListSelectionListener() {
+
+					@Override
+					public void valueChanged(ListSelectionEvent event) {
+						Object object = mainList.getSelectedValue();
+						URL newDocument = websiteInfo.get(object);
+						AppletContext browser = getAppletContext();
+					}
+					
+				}
+				);
+		
+	}
+
+	private void grabHTMLInfo() {
 		
 		
 	}
