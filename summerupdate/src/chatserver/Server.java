fdb577573314e16a7260b683a63339e922d9d5df
jinxtelesis <dremon7@hotmail.com>
2018-06-15 18:26:20 -0400

    streams set up

--- summerupdate/src/chatserver/Server.java
+++ summerupdate/src/chatserver/Server.java
@@ -1,7 +1,21 @@
 package chatserver;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Server extends JFrame{
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
+	// hmm object output stream does???
+	// this is the ip 
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
 
 }
