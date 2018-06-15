06e689f108737d2c25bb79b265d9a79dbff79e02
jinxtelesis <dremon7@hotmail.com>
2018-06-15 14:48:59 -0400

    update

--- summerupdate/src/thread/dre.java
+++ summerupdate/src/thread/dre.java
@@ -4,6 +4,18 @@ public class dre {
 
 	public static void main(String[] args) {
 		
+		Thread t1 =new Thread(new Secondary("one"));
+		// thread needs runnable
+		Thread t2 = new Thread(new Secondary("two"));
+		
+		Thread t3 = new Thread(new Secondary("three"));
+		
+		Thread t4 = new Thread(new Secondary("four"));
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 
 	}
 
