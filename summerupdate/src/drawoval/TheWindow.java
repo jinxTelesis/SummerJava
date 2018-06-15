5d419cfcfb27730fa4d72a56343069fa182308cb
jinxtelesis <dremon7@hotmail.com>
2018-06-15 13:36:02 -0400

    built jFrame

--- summerupdate/src/drawoval/TheWindow.java
+++ summerupdate/src/drawoval/TheWindow.java
@@ -1,5 +1,26 @@
 package drawoval;
 
-public class TheWindow {
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+
+public class TheWindow extends JFrame {
+	
+	private JSlider slider;
+	private DrawOval myPanel;
+	
+	public TheWindow() {
+		super("The title");
+		myPanel = new DrawOval();
+		myPanel.setBackground(Color.lightGray);
+		
+		slider = new JSlider(SwingConstants.HORIZONTAL, 0, 200, 10);
+		slider.setMajorTickSpacing(10);
+		slider.setPaintTicks(true);
+		
+		
+	}
 
 }
