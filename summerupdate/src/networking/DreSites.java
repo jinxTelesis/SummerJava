12765f37278677681185f0b1de3f9476d01da81a
jinxtelesis <dremon7@hotmail.com>
2018-06-15 15:08:41 -0400

    networking example

---
+++ summerupdate/src/networking/DreSites.java
@@ -0,0 +1,16 @@
+package networking;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JApplet;
+import javax.swing.JList;
+
+public class DreSites extends JApplet {
+	
+	private HashMap<String, URL> websiteInfo;
+	private ArrayList<String> titles;
+	private JList mainList;
+
+}
