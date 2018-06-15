6cfd8cb0aee73fea4587981b452c40bd01dbc53f
jinxtelesis <dremon7@hotmail.com>
2018-06-15 13:14:36 -0400

    simple applet

--- summerupdate/src/summerupdate/Dre.java
+++ summerupdate/src/summerupdate/Dre.java
@@ -3,12 +3,26 @@ package summerupdate;
 import java.awt.Graphics;
 
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Dre extends JApplet {
 	
-	public void paint(Graphics g) {
-		super.paint(g);
-		g.drawString("wow this actually worked?", 25, 25);
+	private double sum;
+	
+	public void init() {
+		String fn = JOptionPane.showInputDialog("Enter first number");
+		String sn = JOptionPane.showInputDialog("Enter second number");
+		
+		double n1 = Double.parseDouble(fn);
+		double n2 = Double.parseDouble(sn);
+		
+		sum = n1 + n2;
+		
 	}
+		public void paint(Graphics g) {
+			super.paint(g);
+			g.drawString("The sum is ", 25, 30);
+		}
+		
 
 }
