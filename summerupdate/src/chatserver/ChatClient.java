d076ee994133ae3d08ab6dfa853ab6ff5ee6f74b
jinxtelesis <dremon7@hotmail.com>
2018-06-16 15:04:46 -0400

    bunch of unimplemented methods

--- summerupdate/src/chatserver/ChatClient.java
+++ summerupdate/src/chatserver/ChatClient.java
@@ -1,5 +1,62 @@
 package chatserver;
 
-public class ChatClient {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
+public class ChatClient extends JFrame{
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private String serverIP;
+	private Socket connection;
+	
+	//constructor
+	public ChatClient(String host){
+		super("Client dude");
+		serverIP = host;
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent event){
+						sendData(event.getActionCommand());
+					userText.setText("");
+					
+					}
+	     	}
+		);
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+		setSize(300,150);
+		setVisible(true);
+	}
+	
+	public void startRunning() {
+		try {
+			connectToServer();
+			setupStreams();
+			whileChatting();		}
+		catch (EOFException eofException) {
+			showMessage("\n Client terminated connection");
+		}
+		catch(IOException ioException) {
+		ioException.printStackTrace();
+		}finally {
+			closeCrap();
+		}
+	}
+	
 }
