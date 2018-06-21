39c8d1122c44aca29dba6c10aefc4ee655bfe9a0
jinxtelesis <dremon7@hotmail.com>
2018-06-21 15:08:17 -0400

    window shows

--- memoryGameProject2/src/version1/Driver.java
+++ memoryGameProject2/src/version1/Driver.java
@@ -1,11 +1,26 @@
 package version1;
 
+import javax.swing.JFrame;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				memJFrame frame = new memJFrame();
+				frame.setVisible(true);
+				frame.setSize(400,400);
+				frame.setResizable(false);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			}
+			
+		});
 		
-
+		
 	}
 
 }
