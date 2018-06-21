0acef5ab69754c0839b7b2855e85917757fcbca6
jinxtelesis <dremon7@hotmail.com>
2018-06-21 15:18:29 -0400

    updated layout and borders

--- memoryGameProject2/src/version1/memJPanelBase.java
+++ memoryGameProject2/src/version1/memJPanelBase.java
@@ -1,17 +1,57 @@
 package version1;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
 
 public class memJPanelBase extends JPanel {
 	
 	memJPanelBase(){
+	init();	// method for construction
+	
+	
+	
+	
+	}
+
+	private void init() {
+		Border blackline, raisedetched, loweredetched,
+		raisedbevel, loweredbevel, empty;
+
+		blackline = BorderFactory.createLineBorder(Color.black);
+		raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+		loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		raisedbevel = BorderFactory.createRaisedBevelBorder();
+		loweredbevel = BorderFactory.createLoweredBevelBorder();
+		empty = BorderFactory.createEmptyBorder();
+		
+		FlowLayout flowLayObj = new FlowLayout();
+		
 		memJPanelUpper jpUpper = new memJPanelUpper();
+		jpUpper.setBorder(blackline);
+		jpUpper.setLayout(flowLayObj);
+		
+		GridLayout GridLayObj = new GridLayout(5,7);
 		memJPanelMid jpMid = new memJPanelMid();
+		jpMid.setLayout(GridLayObj);
+		jpMid.setBorder(blackline);
+		
+		
 		memJPanelBot jpBot = new memJPanelBot();
+		jpBot.setLayout(flowLayObj);
+		jpBot.setBorder(blackline);
+		
 		
 		add(jpUpper);
 		add(jpMid);
 		add(jpBot);
+		
 	}
 
 }
