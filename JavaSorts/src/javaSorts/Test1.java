f41aa78f3e902340be35f24abfe4d048632be311
jinxtelesis <dremon7@hotmail.com>
2018-06-27 16:39:05 -0400

    merge sort example

---
+++ JavaSorts/src/javaSorts/Test1.java
@@ -0,0 +1,29 @@
+package javaSorts;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int array1[] = {100,98,77,90,7,101,10,50};
+		
+		printArray(array1);
+		
+		
+
+	}
+	
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for(int i =0; i<n;i++)
+		{
+			System.out.println(arr[i] + " ");
+			
+		}
+		System.out.println();
+		
+		
+	}
+
+}
+
+
