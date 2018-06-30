f82ae4c247c3f0c657b1b6058b2cd8fdbdad679c
jinxtelesis <dremon7@hotmail.com>
2018-06-30 18:22:19 -0400

    continue statement

--- javaBookMath/src/javaBookMath/Main.java
+++ javaBookMath/src/javaBookMath/Main.java
@@ -3,49 +3,14 @@ package javaBookMath;
 public class Main {
 
 	public static void main(String[] args) {
-		long startTime;
-		long endTime;
-		double time;
 		
-		String advice;
-		advice = "Seize the day!";
-		String badAdvice;
-		badAdvice = "Play it safe";
-		
-		System.out.println(advice.equals(badAdvice));
-		System.out.println(advice.equalsIgnoreCase(badAdvice));
-		System.out.println(advice.length());
-		System.out.println(advice.charAt(1));
-		System.out.println(advice.indexOf(badAdvice));
-		advice.toUpperCase();
-		advice.toLowerCase();
-		advice.trim();
-		
-		startTime = System.currentTimeMillis();
-		
-		double width, height, hypotenuse;
-		width = 42.0;
-		height = 17.0;
-		hypotenuse = Math.sqrt(width*width + height*height);
-		System.out.print("A triangle with sides 42 and 17 has a hypotenuse ");
-		System.out.println(hypotenuse);
-		
-		System.out.println("\nMathematically, sin(x)*sin(x) + "
-				+ "cos(x)*cos(x) - 1 should be 0.");
-		
-		System.out.println(Math.random());
-		System.out.println("test1");
-		System.out.print("The value of Math.PI is");
-		
-		endTime = System.currentTimeMillis();
-		time = (endTime - startTime) / 1000.0;
-		
-		System.out.print("\nRun time in seconds was: ");
-		
-		System.out.println(time);
-		
-		
-		
+		System.out.println("i sqrt(i)");
+		for(int i = 1; i <=5; ++i) {
+			if(i == 4) continue;
+		// Rest of loop skipped when i has value of 4
+		System.out.printf("%d     %.2f%n", i,Math.sqrt(i));
+		// Continue with update expression in the loop header
+		}
 
 	}
 
