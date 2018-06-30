a52dc6d9df257669ce1c9df25a898cb41f6caed0
jinxtelesis <dremon7@hotmail.com>
2018-06-30 18:28:31 -0400

    double continue loop

--- javaBookMath/src/javaBookMath/Main.java
+++ javaBookMath/src/javaBookMath/Main.java
@@ -11,7 +11,24 @@ public class Main {
 		System.out.printf("%d     %.2f%n", i,Math.sqrt(i));
 		// Continue with update expression in the loop header
 		}
-
+		
+		
+		int[][] squareMatrix = {{4,3,5},{2,1,6},{9,7,8}};
+		int sum =0;
+		outer: for (int i =0; i < squareMatrix.length;i++) {
+			for(int j = 0; j < squareMatrix[i].length; j++) {
+				if (j == i) continue;
+				System.out.println("Element[" + i + ", " + j + "]: " + squareMatrix[i][j]);
+				sum += squareMatrix[i][j];
+				if (sum > 10) continue outer;
+				// continue goes to innner for loop
+			}
+			// continue outer goes to outer for loop
+		}
+		System.out.println(("Sum: " + sum));
+		
 	}
-
+	
 }
+
+
