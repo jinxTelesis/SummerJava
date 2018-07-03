db1dfad8c3e764fe3b2c9d925065cdab7f9ef9d5
jinxtelesis <dremon7@hotmail.com>
2018-07-03 14:57:08 -0400

    lambda anon, functional java and interfaces

---
+++ LambdaPredicatesEtc/src/FunWithPalindromesV4.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FunWithPalindromesV4 {
+	
+	private static boolean isCaseSensitivePalindrome(String str) {
+		return str.equals(new StringBuilder(str).reverse().toString());
+	}
+	
+	private static boolean isCaseInsensitivePalindrome(String str) {
+		return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
+	}
+	
+	public static void main(String[] args) {
+		
+		List<String> words = new ArrayList<>();
+		words.add("Otto"); words.add("ADA"); words.add("Alyla");
+		words.add("Bob"); words.add("Hannah");
+		
+		List<String> palindromes1 = filterStrings(words,
+				str -> isCaseSensitivePalindrome(str));
+		
+	}
+
+	// not finished
+	private static <E> List<E> filterStrings(List<E> list, Predicate<E> predicate) {
+		
+		List<E> result = new ArrayList<>();
+		for (E element : list)
+		{
+			if (predicate.test(element))
+				result.add(element);
+			
+		}
+		
+		
+		return result;
+	}
+
+}
