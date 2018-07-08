36db4582c8379b0825d75b98c50882dbccb06af4
jinxtelesis <dremon7@hotmail.com>
2018-07-08 19:49:00 -0400

    adding action listeners

--- deleteInterview/src/oop/GuiInterviewJframe.java
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -1,72 +1,58 @@
 package oop;
 
-
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-
+import javax.swing.JTextField;
 
 public class GuiInterviewJframe extends JFrame  {
-	private JLabel label;
+	int randomNum, guess;
 	private JButton button;
-	private JButton button2;
-	private JLabel label2;
+	private JTextField textfield;
+	private JLabel promptLabel;
+	private JLabel resultLabel;
+	private JLabel randomLabel;
 	
 	public GuiInterviewJframe() {
-		setLayout(new FlowLayout());
+		promptLabel = new JLabel("Enter a random number 1-10");
+		add(promptLabel);
 		
-		button = new JButton("Click for text");
+		textfield = new JTextField(5);
+		add(textfield);
+		
+		button = new JButton("Guess!");
 		add(button);
 		
-		button2 = new JButton("Rocket baby");
-		add(button2);
+		resultLabel = new JLabel("");
+		add(resultLabel);
 		
-		label = new JLabel("");
-		add(label);
+		randomLabel = new JLabel("");
+		add(randomLabel);
 		
-		label2 = new JLabel("");
-		add(label2);
-		
-		event theEvent = new event();
-		button.addActionListener(theEvent);
-		event2 theEvent2 = new event2();
-		button2.addActionListener(theEvent2);
-		
+		event e = new event();
+		button.addActionListener(e);
 	}
 	
 	public class event implements ActionListener{
 
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			label.setText("Now you can see words here");
+		public void actionPerformed(ActionEvent e) {
+			
+			
 		}
 		
 	}
 	
-	public class event2 implements ActionListener{
+	
+	
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			label2.setText("Blast off to the moon baby");
-		}
-		
-	}
 	
-	public static void main(String args[])
-	{
-		GuiInterviewJframe gui = new GuiInterviewJframe();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setTitle("Event listeners");
-		gui.setSize(300,1000);
-		gui.setVisible(true);
-		
-	}
+	
+	
+	
 	
 	
 
