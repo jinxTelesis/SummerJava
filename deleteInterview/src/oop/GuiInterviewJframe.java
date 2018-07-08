5caabe3ede1b3d6159156a35c17805fce702a6b0
jinxtelesis <dremon7@hotmail.com>
2018-07-08 14:35:18 -0400

    basic gui

--- deleteInterview/src/oop/GuiInterviewJframe.java
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -1,16 +1,36 @@
 package oop;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class GuiInterviewJframe extends JFrame {
 	
+	private JLabel label;
+	private JButton button;
+	private JTextField textfield;
+	
+	
+	public GuiInterviewJframe() {
+		setLayout(new FlowLayout());
+		
+		label = new JLabel("Hi, sup bro");
+		add(label);
+		
+		textfield = new JTextField(15);
+		add(textfield);
+		
+		button = new JButton("Moo");
+		add(button);
+		
+		
+	}
+	
 	public static void main(String args[]) {
 	
-	GuiInterviewJframe gui2 = new GuiInterviewJframe();
-	gui2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	gui2.setSize(400,400);
-	gui2.setVisible(true);
-	gui2.setTitle("ugh");
 	
 	
 	
