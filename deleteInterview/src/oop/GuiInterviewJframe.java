46cf5cd79e89e4d1eee04275d67575a3e3d399ab
jinxtelesis <dremon7@hotmail.com>
2018-07-08 14:53:15 -0400

    ignore file

--- deleteInterview/src/oop/GuiInterviewJframe.java
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -1,11 +1,27 @@
 package oop;
 
 
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 
 public class GuiInterviewJframe extends JFrame  {
+	private ImageIcon image;
+	private JLabel labe1;
+	private ImageIcon image2;
+	private JLabel label2;
+	
+	GuiInterviewJframe(){
+		setLayout(new FlowLayout());
+		
+		image2 = new ImageIcon();
+		
+		
+	}
 	
 	
 	
