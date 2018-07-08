ba96baddc8eddf03c55ef4164b93847772b4d034
jinxtelesis <dremon7@hotmail.com>
2018-07-08 14:40:15 -0400

    updates basic gui elements

--- deleteInterview/src/oop/GuiInterviewJframe.java
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -1,43 +1,13 @@
 package oop;
 
-import java.awt.FlowLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
-public class GuiInterviewJframe extends JFrame {
-	
-	private JLabel label;
-	private JButton button;
-	private JTextField textfield;
-	
-	
-	public GuiInterviewJframe() {
-		setLayout(new FlowLayout());
-		
-		label = new JLabel("Hi, sup bro");
-		add(label);
-		
-		textfield = new JTextField(15);
-		add(textfield);
-		
-		button = new JButton("Moo");
-		add(button);
-		
-		
-	}
-	
-	public static void main(String args[]) {
-		GuiInterviewJframe gui = new GuiInterviewJframe();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setVisible(true);
-		gui.setSize(400,400);
-		
+
+
+public class GuiInterviewJframe extends JFrame  {
 	
 	
 	
-	
-	}
+
 }
