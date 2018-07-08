9807faa0f482f12d237881bddb3e3ed57368e45e
jinxtelesis <dremon7@hotmail.com>
2018-07-08 14:29:12 -0400

    basic gui

---
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -0,0 +1,18 @@
+package oop;
+
+import javax.swing.JFrame;
+
+public class GuiInterviewJframe extends JFrame {
+	
+	public static void main(String args[]) {
+	
+	GuiInterviewJframe gui2 = new GuiInterviewJframe();
+	gui2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	gui2.setSize(400,400);
+	gui2.setVisible(true);
+	gui2.setTitle("ugh");
+	
+	
+	
+	}
+}
