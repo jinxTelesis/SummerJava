8f5355d7cb1fc6e9900b07dcf630fb695aa2697b
jinxtelesis <dremon7@hotmail.com>
2018-07-08 15:02:16 -0400

    just displaying images on image icons on Jlabels

--- deleteInterview/src/oop/GuiInterviewJframe.java
+++ deleteInterview/src/oop/GuiInterviewJframe.java
@@ -11,19 +11,32 @@ import javax.swing.JLabel;
 
 public class GuiInterviewJframe extends JFrame  {
 	private ImageIcon image;
-	private JLabel labe1;
+	private JLabel label1;
 	private ImageIcon image2;
 	private JLabel label2;
 	
 	GuiInterviewJframe(){
 		setLayout(new FlowLayout());
 		
-		image2 = new ImageIcon();
+		image = new ImageIcon(getClass().getResource("images/image0.jpg"));
+		label1 = new JLabel(image);
+		add(label1);
+		
+		image2 = new ImageIcon(getClass().getResource("images/image1.jpg"));
+		label2 = new JLabel(image2);
+		add(label2);
+	}
+	
+	public static void main(String args[]){
+		GuiInterviewJframe gui = new GuiInterviewJframe();
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.setVisible(true);
+		//gui.setSize(500,400);
+		gui.pack();
+		gui.setTitle("Image program baby");
 		
 		
 	}
-	
-	
 	
 
 }
