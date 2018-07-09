4a6e2304a8ef57ce6d58ee355b92f5432f0dd5b2
jinxtelesis <dremon7@hotmail.com>
2018-07-09 17:50:35 -0400

    gridbag layout

--- deleteInterview/src/oop/CalculatorGui.java
+++ deleteInterview/src/oop/CalculatorGui.java
@@ -1,5 +1,8 @@
 package oop;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,6 +15,33 @@ public class CalculatorGui extends JFrame {
 	JLabel result, enter1, enter2;
 	
 	public CalculatorGui() {
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		enter1 = new JLabel("1st:");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(enter1, c);
+		
+		num1 = new JTextField(10);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridwidth = 3;
+		add(num1, c);
+		
+		enter2 = new JLabel("2nd: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.gridwidth = 1;
+		add(enter2, c);
+		
+		
+		
+		
+		
 		
 	}
 
