72cb67eeb26c8c9aedb97a43cf1835cc53f6ddc0
jinxtelesis <dremon7@hotmail.com>
2018-07-09 17:23:29 -0400

    calculator program

---
+++ deleteInterview/src/oop/CalculatorGui.java
@@ -0,0 +1,18 @@
+package oop;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class CalculatorGui extends JFrame {
+	
+	JButton add, subtract, multiply, divide;
+	JTextField num1, num2;
+	JLabel result, enter1, enter2;
+	
+	public CalculatorGui() {
+		
+	}
+
+}
