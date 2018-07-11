d7a43bd734fd4de5870a42c6f132fa925a55c874
jinxtelesis <dremon7@hotmail.com>
2018-07-10 21:44:19 -0400

    confusing constructor

--- BookStuff/src/oop/Test.java
+++ BookStuff/src/oop/Test.java
@@ -3,8 +3,22 @@ package oop;
 public class Test {
 
 	public static void main(String[] args) {
-		NonStaticInitializers hotel = new NonStaticInitializers();
+		Test obj = new Test(n);
 
 	}
+	
+	static int i = 5;
+	static int n;
+	int j = 7;
+	int k;
+	
+	public Test(int m) {
+		System.out.println(i + ", " + j + ", " + k + ", " + n + ", " + m);
+		
+	}
+	
+	{ j = 70; n = 20;}
+	
+	static { i = 50; }
 
 }
