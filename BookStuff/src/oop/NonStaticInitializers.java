c58938c095f14db66479ddc2c0af8554599a39b8
jinxtelesis <dremon7@hotmail.com>
2018-07-10 20:43:39 -0400

    static init

--- BookStuff/src/oop/NonStaticInitializers.java
+++ BookStuff/src/oop/NonStaticInitializers.java
@@ -1,13 +1,15 @@
 package oop;
 
 public class NonStaticInitializers {
-	int length = 10;
-	double area = length * this.width;
-	int width = 10;
 	
-	int sqSide = height = 20;
-	int height;
+	static final int ROWS = 12, COLUMNS = 10;
+	static long[][] matrix = new long[ROWS][COLUMNS];
 	
-
+	static {
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[i].length; j++)
+				matrix[i][j] = 2 * i + j;
+	
+	}
 	
 }
