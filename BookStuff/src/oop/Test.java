aa70c17d947740689286f563e030b9573a92335d
jinxtelesis <dremon7@hotmail.com>
2018-07-13 22:59:49 -0400

    arrayList added

--- BookStuff/src/oop/Test.java
+++ BookStuff/src/oop/Test.java
@@ -9,14 +9,22 @@ public class Test {
 
 	public static void main(String[] args) {
 	
-		ArrayList<String> palindromes = new ArrayList<String>();
-		ArrayList<String> identifier = new ArrayList<String>();
-		ArrayList<String> indenifier2 = new ArrayList<String>();
-		ArrayList<String> indentifier3 = new ArrayList<String>();
+		ArrayList<String> palindromes = new ArrayList<String>(20);
+		ArrayList<Integer> intList = new ArrayList<>();
+		intList.add(10); intList.add(100); intList.add(1000);
 		
-		ArrayList<String> id3 = new ArrayList<>(20);
+		palindromes.add("level"); palindromes.add("Ada"); palindromes.add("kayak");
+		System.out.println(palindromes);
 		
-		List<String> palindromes2 = new LinkedList<String>();
+		List<String> wordList = new ArrayList<>(palindromes);
+		System.out.println(wordList);
+		
+		List<StringBuilder> synonyms = new ArrayList<>();
+		List<Integer> attendence = new ArrayList<>();
+		List<List<String>> ListOfLists = new ArrayList<>();
+		
+		Object[] objArray = new String[10];
+		
 
 	}
 	
