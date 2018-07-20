6d6f7fcd66000ab61c91b88f318ce111ffb16449
jinxtelesis <dremon7@hotmail.com>
2018-07-20 19:55:45 -0400

    Improved look

--- XMLStorage/src/gui_int/Login.java
+++ XMLStorage/src/gui_int/Login.java
@@ -6,18 +6,30 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JPasswordField;
 
 public class Login {
 
 	private JFrame frame;
 	private JTextField textField;
-	private JTextField textField_1;
+	private JPasswordField passwordField_1;
+	
 
 
 	public static void main(String[] args) {
+		
+	    try {
+	        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+	    } catch(Exception e) {
+	        System.out.println("Error setting native LAF: " + e);
+	    }
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -26,6 +38,8 @@ public class Login {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				
 			}
 		});
 	}
@@ -35,6 +49,7 @@ public class Login {
 	}
 
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 400, 280);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,16 +65,15 @@ public class Login {
 		frame.getContentPane().add(lblPassword);
 		
 		textField = new JTextField();
-		textField.setBounds(145, 54, 137, 20);
+		textField.setBounds(145, 54, 137, 25);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(145, 99, 137, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnLogin.setBounds(64, 146, 89, 23);
 		frame.getContentPane().add(btnLogin);
 		
@@ -70,5 +84,11 @@ public class Login {
 		});
 		btnExit.setBounds(193, 146, 89, 23);
 		frame.getContentPane().add(btnExit);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(145, 99, 137, 25);
+		frame.getContentPane().add(passwordField_1);
+		
+	
 	}
 }
