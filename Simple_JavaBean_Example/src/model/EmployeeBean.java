de13e5952a81a819dfd16b9c9d33e56a9f8e5c0d
jinxtelesis <dremon7@hotmail.com>
2018-07-31 21:34:15 -0400

    java bean example

---
+++ Simple_JavaBean_Example/src/model/EmployeeBean.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.io.Serializable;
+
+public class EmployeeBean implements Serializable {
+	
+	// java bean so private fields with getters and setters standard
+	// default no arg constructor
+	private String firstName;
+	private String lastName;
+
+}
